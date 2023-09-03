aa91cc0c3f3089aa12663e18f627c3b986491c7b
matthewsvale <matthewsvale@gmail.com>
2023-09-03 02:00:09 -0700

    markers show up on the map but need to make it interactive

--- 2app.py
+++ 2app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import Column, Integer, String, Float
 
 db = SQLAlchemy()
 
-class Marker(db.Model):
+class Airbnb(db.Model):
+    __tablename__ = 'airbnbs'
     id = db.Column(db.Integer, primary_key=True)
-    neighborhood = db.Column(db.String(255))
-    latitude = db.Column(db.Integer)
-    longitude = db.Column(db.Integer)
+    neighbourhood = db.Column(db.String(255))
+    latitude = db.Column(db.Numeric)
+    longitude = db.Column(db.Numeric)
     room_type = db.Column(db.String(255))
-    price = db.Column(db.Float)
+    price = db.Column(db.Numeric)
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/vacation_safety_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mypostgres:postgres@localhost:5432/vacation_safety_db'
 db.init_app(app)
 
 
@@ -23,14 +24,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/markers')
-def markers():
+@app.route('/airbnb')
+def airbnb_data():
 
-    markers = Marker.query.all()
-    markers_data = [{'neighbourhood': marker.neighbourhood, 'latitude': marker.latitude, 'longitude': marker.longitude, 'room_type': marker.room_type, 'price': marker.price} for marker in markers]
-    print(markers_data)
+    try:
+        airbnb_data = Airbnb.query.all()
+
+        results = [{'neighbourhood': data.neighbourhood, 'latitude': data.latitude, 'longitude': data.longitude, 'room_type': data.room_type, 'price': data.price} for data in airbnb_data]
+        print(results)
     
-    return jsonify(markers_data)
+        return jsonify(results)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- static/js/logic.js
+++ static/js/logic.js
@@ -5,15 +5,26 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-fetch('/markers')
 
-    .then(response => response.json())
-    .then(data => {
-        // Loop through the marker data and add markers to the map
-        console.log(data)
-        data.forEach(marker => {
-            L.marker([marker.latitude, marker.longitude])
-                .addTo(map)
-                .bindPopup(marker.neighbourhood);  // Show marker name as a popup
+const apiEndpoint = '/airbnb';
+
+function fetchData() {
+    fetch(apiEndpoint)
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach(item => {
+            var marker = L.marker([item.latitude, item.longitude]).addTo(map);
+            // Customize the marker as needed, e.g., add pop-up content
+            marker.bindPopup(`<strong>${item.price}</strong><br>Neighbourhood: ${item.neighbourhood}`);
+
         });
-    });
+        // Process and use the data as needed
+        console.log(data); // Example: Log the data to the console
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  
+  // Call the fetchData function to retrieve data from the new endpoint
+  fetchData();
